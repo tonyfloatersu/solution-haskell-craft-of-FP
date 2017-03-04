d5a89e56af534c8308fd95afd44a74a9310a25e2
tonyfloatersu <tonyfloater@gmail.com>
2017-03-04 15:51:06 +0800

    chapter8 3	left 2017/3/4

--- Chapter_8_my_note.hs
+++ Chapter_8_my_note.hs
@@ -4,26 +4,41 @@ import           Chapter_7_my_note (isPalin, splitWord, qSort)
 import           Data.Time.Clock
 import           Data.Time.Format
 import           System.IO.Unsafe
+import           Test.QuickCheck
 
 data Move = Rock | Paper | Scissors
-            deriving (Show, Eq)
+            deriving Eq
+
+instance Show Move where
+         show Rock        = "r"
+         show Paper       = "p"
+         show Scissors    = "s"
+
+convertMove :: Char -> Move
+convertMove 'r'    = Rock
+convertMove 'R'    = Rock
+convertMove 'p'    = Paper
+convertMove 'P'    = Paper
+convertMove 's'    = Scissors
+convertMove 'S'    = Scissors
+convertMove _      = error "do not match"
 
 type Tournament = ([Move], [Move])
 
 outcome :: Move -> Move -> Integer
-outcome Rock Paper     = -1
-outcome Paper Rock     = 1
-outcome Paper Scissors = -1
-outcome Rock Scissors  = 1
-outcome Scissors Paper = 1
-outcome Scissors Rock  = -1
-outcome _ _            = 0
+outcome Rock Paper        = -1
+outcome Paper Rock        = 1
+outcome Paper Scissors    = -1
+outcome Rock Scissors     = 1
+outcome Scissors Paper    = 1
+outcome Scissors Rock     = -1
+outcome _ _               = 0
 
 tournamentOutcome :: Tournament -> Integer
 tournamentOutcome tour    = sum [outcome (fst tp) (snd tp)
                                  | tp <- (zip (fst tour) (snd tour))]
 
-type Strategy = [Move] -> Move
+type Strategy    = [Move] -> Move
 
 rock, paper, scissors :: Strategy
 rock _        = Rock
@@ -187,7 +202,8 @@ ioSumList    = do
     putStrLn ("Give me " ++ show quantity ++ " number(s): ")
     list <- getAllInt quantity
     let listsum = sum list
-    putStrLn ("Here is the sum of " ++ show quantity ++ "number(s): " ++ show listsum)
+    putStrLn ("Here is the sum of " ++ show quantity
+              ++ "number(s): " ++ show listsum)
 
 copyEmpty :: IO ()
 copyEmpty    = do
@@ -289,3 +305,22 @@ copy =    do
 -- the immitation to while is wrong here
 -- since line is predefined for the whileCopy,
 -- so it will never change variable line twice
+
+showResults :: Tournament -> IO ()
+showResults tour    = do
+    let res    = tournamentOutcome tour
+    putStrLn (case compare res 0 of
+                   GT -> "I won!"
+                   EQ -> "Draw!"
+                   LT -> "You won!")
+
+playInteractive :: Strategy -> Tournament -> IO ()
+playInteractive strategy tnmt@ (mine, yours)    = do
+    ch <- getChar
+    if not (ch `elem` "rpsRPS")
+        then showResults tnmt
+        else do
+            let next    = strategy yours
+            putStrLn ("\nI play: " ++ show next ++ " you play: " ++ [ch])
+            let yourmove    = convertMove ch
+            playInteractive strategy (next : mine, yourmove : yours)
