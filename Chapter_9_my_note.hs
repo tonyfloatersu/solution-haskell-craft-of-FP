66581c459120e8785bd2613fbad25380389d4ec5
tonyfloatersu <tonyfloater@gmail.com>
2017-03-06 17:52:27 +0800

    chapter9 start 2017/3/6

--- Chapter_9_my_note.hs
+++ Chapter_9_my_note.hs
@@ -1,3 +1,67 @@
 module Chapter_9_my_note where
 
+import           Pictures
 
+frac :: Integer -> Integer
+frac n
+    | n == 0       = 1
+    | otherwise    = n * frac (n - 1)
+
+mult :: Integer -> Integer -> Integer
+mult a b
+    | a == 0 || b == 0    = 0
+    | otherwise           = a * b
+-- see that static finding, so check a first then check b
+
+{-
+solution 9.3
+-}
+
+{-
+solution 9.4
+-}
+
+{-
+solution 9.5
+-}
+
+{-
+solution 9.6
+-}
+
+{-
+solution 9.7
+-}
+
+{-
+solution 9.8
+-}
+
+{-
+solution 9.9
+-}
+
+{-
+solution 9.10
+-}
+
+{-
+solution 9.11
+-}
+
+
+{-
+solution 9.11
+-}
+
+{-
+solution 9.14
+-}
+
+{-
+solution 9.15
+-}
+
+{-
+solution 9.16
+-}
