e2b524389d7a0f98370b79db7c351e8df22c779f
tonyfloatersu <tonyfloater@gmail.com>
2017-03-21 16:51:59 +0800

    chapter14 14.4 2017/3/21

--- Chapter_14_my_note.hs
+++ Chapter_14_my_note.hs
@@ -24,8 +24,40 @@ sumTree, depth :: Ntree -> Integer
 sumTree NilT                      = 0
 sumTree (Node val tree1 tree2)    = val + sumTree tree1 + sumTree tree2
 depth NilT                        = 0
-depth (Node _ tree1 tree2)      = 1 + max (depth tree1) (depth tree2)
+depth (Node _ tree1 tree2)        = 1 + max (depth tree1) (depth tree2)
 
 occurs :: Ntree -> Integer -> Integer
 occurs NilT _    = 0
 occurs (Node val t1 t2) p    = (if val == p then 1 else 0) + occurs t1 p + occurs t2 p
+
+assoc :: Expr -> Expr
+assoc (Add (Add e1 e2) e3)    = assoc (Add e1 (Add e2 e3))
+assoc (Add e1 e2)             = Add (assoc e1) (assoc e2)
+assoc (Sub e1 e2)             = Sub (assoc e1) (assoc e2)
+assoc (Lit fa)                = Lit fa
+
+sizeepr :: Expr -> Integer
+sizeepr (Lit _)            = 0
+sizeepr (Sub exp1 exp2)    = 1 + sizeepr exp1 + sizeepr exp2
+sizeepr (Add exp1 exp2)    = 1 + sizeepr exp1 + sizeepr exp2
+
+data Express = Literal Integer
+             | Express :+: Express
+             | Express :-: Express
+
+evaluation :: Express -> Integer
+evaluation (Literal valu)     = valu
+evaluation (exp1 :+: exp2)    = evaluation exp1 + evaluation exp2
+evaluation (exp1 :-: exp2)    = evaluation exp1 - evaluation exp2
+
+instance Show Express where
+    show (Literal val)        = show val
+    show (exp1 :+: exp2)      = "(" ++ show exp1 ++ " + " ++ show exp2 ++ ")"
+    show (exp1 :-: exp2)      = "(" ++ show exp1 ++ " - " ++ show exp2 ++ ")"
+
+assocr :: Express -> Express
+assocr ((e1 :+: e2) :+: e3)    = assocr (e1 :+: (e2 :+: e3))
+assocr (e1 :+: e2)             = assocr e1 :+: assocr e2
+assocr (e1 :-: e2)             = assocr e1 :-: assocr e2
+assocr (Literal val)           = Literal val
+
