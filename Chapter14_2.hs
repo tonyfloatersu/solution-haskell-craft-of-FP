39b6244c6c576f2296b038596031fdcef9ff0c53
tonyfloatersu <tonyfloater@gmail.com>
2017-03-23 23:36:13 +0800

    chapter14 done 2017/3/23

--- Chapter14_2.hs
+++ Chapter14_2.hs
@@ -3,7 +3,7 @@
 --  Haskell: The Craft of Functional Programming, 3e
 --  Simon Thompson
 --  (c) Addison-Wesley, 1996-2011.
--- 
+--
 --  Chapter 14, part 2
 --      Details of the Simulation case study in the Simulation directory.
 --
