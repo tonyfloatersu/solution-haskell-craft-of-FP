fa31dd884712cb2204c392841ee2aefec443bed4
tonyfloatersu <tonyfloater@gmail.com>
2017-03-25 09:58:17 +0800

    chapter15 done 2017/3/24

--- CodeTable.hs
+++ CodeTable.hs
@@ -9,5 +9,5 @@ codeTable :: Tree -> Table
 codeTable    = convert []
 
 convert :: HCode -> Tree -> Table
-convert hc (Leaf c _)    = [(c, hc)]
+convert hc (Leaf c _)        = [(c, hc)]
 convert hc (Node _ t1 t2)    = convert (hc ++ [L]) t1 ++ convert (hc ++ [R]) t2
