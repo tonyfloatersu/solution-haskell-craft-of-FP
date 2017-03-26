6a958c6e0481ff61e4ea904435a337badcb65d70
tonyfloatersu <tonyfloater@gmail.com>
2017-03-26 13:35:43 +0800

    chapter16 16.5 done 2017/3/26

--- Chapter_16_my_note.hs
+++ Chapter_16_my_note.hs
@@ -1,1 +1,21 @@
 module Chapter_16_my_note where
+
+import           StoreSeq ( StoreS
+                          , initialS
+                          , updateS
+                          , valueS )
+
+import           Store ( Store
+                       , initial
+                       , update
+                       , value
+                       , equstore
+                       , valueEM
+                       , valueE
+                       , valueM )
+
+import           StoreF ( StoreF
+                        , initialF
+                        , updateF
+                        , valueF
+                        , setAll )
