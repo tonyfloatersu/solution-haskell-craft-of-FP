e3979fe8711c35b220d10bfcc3e20f53948e7705
tonyfloatersu <tonyfloater@gmail.com>
2017-03-30 17:46:50 +0800

    chapter16 update 2017/3/30

--- MTree.hs
+++ MTree.hs
@@ -18,8 +18,6 @@ module MTree ( MTree
 -- decode message with minTree maxTree element_occurance
 
 data Decode a = Decode { element_nd :: a
-                       , maxTree :: a
-                       , minTree :: a
                        , occurance :: Int }
                        deriving (Eq, Show)
 
@@ -32,3 +30,17 @@ misNil :: MTree a -> Bool
 misNil MNil        = False
 misNil MNode {}    = True
 
+misNode :: MTree a -> Bool
+misNode    = not . misNil
+
+mleftSub :: MTree a -> MTree a
+mleftSub MNil             = MNil
+mleftSub (MNode _ l _)    = l
+
+mrightSub :: MTree a -> MTree a
+mrightSub MNil             = MNil
+mrightSub (MNode _ _ r)    = r
+
+mtreeVal :: MTree a -> Maybe a
+mtreeVal MNil                        = Nothing
+mtreeVal (MNode (Decode v _) _ _)    = Just v
