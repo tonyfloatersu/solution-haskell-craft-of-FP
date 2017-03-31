63c3669c5ff05e713a4d2477a2506a1a1ba84e7e
tonyfloatersu <tonyfloater@gmail.com>
2017-03-31 19:41:53 +0800

    chapter16 relation done 2017/3/31

--- Relation.hs
+++ Relation.hs
@@ -2,12 +2,13 @@ module Relation ( Relation
                 , image
                 , setImage
                 , addImage
---                , addChildren
+                , addChildren
                 , compose
                 , setProduct
                 , adjoin
                 , tClosure
-                , limit ) where
+                , limit
+                , connect ) where
 
 import           SetADT
 
@@ -23,7 +24,8 @@ addImage :: Ord a => Relation a -> Set a -> Set a
 addImage ret st    = st `union` setImage ret st
 
 compose :: Ord a => Relation a -> Relation a -> Relation a
-compose rel1 rel2    = mapSet (\((a, _), (_, d)) -> (a, d)) (setProduct rel1 rel2)
+compose rel1 rel2    = mapSet (\((a, _), (_, d)) -> (a, d))
+                       (filterSet (\((_, b), (c, _)) -> b == c) (setProduct rel1 rel2))
 
 setProduct :: (Ord a, Ord b) => Set a -> Set b -> Set (a, b)
 setProduct st1    = setUnion . mapSet (adjoin st1)
@@ -38,3 +40,23 @@ limit :: Eq a => (a -> a) -> a -> a
 limit f x
     | x == f x     = x
     | otherwise    = limit f (f x)
+
+type People = String
+
+isParents :: Relation People
+isParents    = makeSet [("Ben", "Sue"), ("Ben", "Leo"), ("Sue", "Joe")]
+
+isGrandParents :: Relation People
+isGrandParents    = isParents `compose` isParents
+
+inverser :: Ord a => Relation a -> Relation a
+inverser    = mapSet (\(a, b) -> (b, a))
+
+addChildren :: Set People -> Set People
+addChildren    = addImage isParents
+
+connect :: Ord a => Relation a -> Relation a
+connect rel    = tClosure rel `union` (inverser . tClosure) rel
+
+elems :: Ord a => Relation a -> Set a
+elems rel    = mapSet fst rel `union` mapSet snd rel
