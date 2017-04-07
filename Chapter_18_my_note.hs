5fd5769287b23283718c6b23b6c7aeb4ce1f2e72
tonyfloatersu <tonyfloater@gmail.com>
2017-04-07 09:00:06 +0800

    chapter18 18.3 done 2017/4/7

--- Chapter_18_my_note.hs
+++ Chapter_18_my_note.hs
@@ -1,1 +1,35 @@
 module Chapter_18_my_note where
+
+readAndWrite :: IO ()
+readAndWrite    = do line <- getLine
+                     putStrLn "Get one line"
+                     putStrLn line
+
+readEcho :: IO ()
+readEcho    = do putStrLn "Give me a line"
+                 line <- getLine
+                 putStrLn ("the line we get is: " ++ line)
+
+sumInts :: Integer -> IO Integer
+sumInts s    = do temp <- getLine
+                  let n = read temp :: Integer
+                  if n == 0
+                  then return s
+                  else sumInts (s + n)
+
+sumInteract :: IO ()
+sumInteract    = do putStrLn "Enter Integers one per line"
+                    putStrLn "These will be summed until zero occurred"
+                    sum_val <- sumInts 0
+                    putStrLn ("The final sum is " ++ show sum_val)
+
+fmap :: (a -> b) -> IO a -> IO b
+fmap f origio    = do val <- origio
+                      return (f val)
+
+repeater :: IO Bool -> IO () -> IO ()
+repeater test oper    = do checker <- test
+                           if checker
+                           then return ()
+                           else do oper
+                                   repeater test oper
