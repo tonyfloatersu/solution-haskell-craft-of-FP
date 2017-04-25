736f11bc41f5a84266151443982cf44425e11e92
tonyfloatersu <tonyfloater@gmail.com>
2017-04-25 20:09:00 +0800

    calculator interpreter 40%

--- CalcInptStoreModule.hs
+++ CalcInptStoreModule.hs
@@ -1,7 +1,18 @@
-module CalcInptStoreModule () where
+module CalcInptStoreModule ( Store, initial, valueS, update ) where
 
 import           CalcInptType ( Expr (..), Ops (..), Parse )
 
 type Var = String
 
 newtype Store = Store [(Expr, Var)]
+
+initial :: Store
+initial    = Store []
+
+valueS :: Store -> Var -> Expr
+valueS (Store []) _                           = Var "Fail"
+valueS (Store((e, v) : xs)) vt | vt == v      = e
+                               | otherwise    = valueS (Store xs) vt
+
+update :: Store -> Var -> Expr -> Store
+update (Store st) v e    = Store ((e, v) : st)
